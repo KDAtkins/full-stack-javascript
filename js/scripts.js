df9f6841106521821d6437c618ef590486ed3691
Kevin Atkins <kevindewayneatkins@gmail.com>
2019-05-07 09:41:04 -0700

    minor changes to display on 'index.html'

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-document.write("<h1> Welcome to Javascript Basics</h1>");
-alert('Thanks for visiting!');
+// write statements to either present on the webpage or to make an alert message via dialog box
+// alert('Hello World');
+// document.write("<h1> Welcome to Javascript Basics</h1>");
+// alert('Thanks for visiting!');
+
+
+alert("Hey, you're back for more?");
