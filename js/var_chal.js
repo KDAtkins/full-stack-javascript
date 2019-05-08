05f8a3d56a83b115e06a8f6d0439cf78aa46b14c
Kevin Atkins <kevindewayneatkins@gmail.com>
2019-05-08 11:59:53 -0700

    added code to countdown remaining questions

--- js/var_chal.js
+++ js/var_chal.js
@@ -1,6 +1,14 @@
-var verb = prompt("Enter a verb:");
-var noun = prompt("Enter a noun:");
-var adjective = prompt("Enter an adjective:");
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]';
+
+var verb = prompt("Enter a verb: " + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt("Enter a noun: " + questionsLeft);
+questions -= 1;
+var questionsLeft = ' [' + questions + ' questions left]';
+var adjective = prompt("Enter an adjective: " + questionsLeft);
+
 
 alert("You're done!");
 
