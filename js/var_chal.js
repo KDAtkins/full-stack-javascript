03e63eb333bcb110e38e8c8ba02146bc00ef5f86
Kevin Atkins <kevindewayneatkins@gmail.com>
2019-05-08 10:16:02 -0700

    completed the 'Variable Challenge' aka 'The Story Maker' challenge

---
+++ js/var_chal.js
@@ -0,0 +1,9 @@
+var verb = prompt("Enter a verb:");
+var noun = prompt("Enter a noun:");
+var adjective = prompt("Enter an adjective:");
+
+alert("You're done!");
+
+var story = "There was once a " + noun + " that " + verb + " to a " + adjective + " paradise.";
+
+document.write(story);
